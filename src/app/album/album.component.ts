db727fda3dcca55f0ef79c742bdd50ab49f34f76
Mark Culp <markculp0@gmail.com>
2019-12-29 21:32:17 -0600

    album 4, add year

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -13,6 +13,7 @@ export class AlbumComponent implements OnInit {
       id: '0',
       title: 'Surfacing',
       artist: 'Sarah McLauchlan',
+      year: '1997',
       image: '/assets/images/surfacing.png',
       featured: true,
       description: 'A defining album for an incredible talent.'
@@ -21,17 +22,28 @@ export class AlbumComponent implements OnInit {
       id: '1',
       title: 'Netherlands',
       artist: 'Dan Fogelberg',
+      year: '1977',
       image: '/assets/images/netherlands.png',
       featured: false,
-      description: 'One of Dan\'s best.  Introspective and profound.' 
+      description: 'One of Dan\'s best.  Introspective and emotional.'
     },
     {
       id: '2',
       title: 'X & Y',
       artist: 'Coldplay',
+      year: '2005',
       image: '/assets/images/xAndY.png',
       featured: false,
-      description: 'Hooked me for life.  Coldplay is hypnotic.'
+      description: 'Coldplay is hypnotic.  This is one of their best.'
+    },
+    {
+      id: '3',
+      title: 'Eldorado',
+      artist: 'Electric Light Orchestra',
+      year: '1974',
+      image: '/assets/images/eldorado.png',
+      featured: false,
+      description: 'I had never heard anything like this before.  ELO\'s most serious work.'
     }
   ];
 
--- src/app/shared/disc.ts
+++ src/app/shared/disc.ts
@@ -2,6 +2,7 @@ export class Disc {
     id: string;
     title: string;
     artist: string;
+    year: string;
     image: string;
     featured: boolean;
     description: string;
