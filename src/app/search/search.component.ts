d8ea96bcd8fb085d794b1bea4fa2b7c39eae245b
Mark Culp <markculp0@gmail.com>
2020-02-09 10:35:59 -0600

    search by id

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Router} from '@angular/router';
 
 import { Disc } from '../shared/disc';
+import { DiscService } from '../services/disc.service';
 
 @Component({
   selector: 'app-search',
@@ -14,13 +16,23 @@ export class SearchComponent implements OnInit {
   discsearchForm: FormGroup;
   disc: Disc;
 
+  discs: Disc[];
+  selectedDisc: Disc;
+
+  // Current shared discs
+  curDisc: Disc;
+  curDiscs: Disc[];
+
   @ViewChild('fform', {static: false} ) feedbackFormDirective: any;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private discService: DiscService, private router: Router) {
     this.createForm();
   }
 
   ngOnInit() {
+
+    this.discService.getDiscs()
+    .subscribe(discs => this.discs = discs);
   }
 
   createForm() {
@@ -31,12 +43,25 @@ export class SearchComponent implements OnInit {
 
   onSubmit() {
     this.disc = this.discsearchForm.value;
-    console.log(this.disc);
+    console.log(this.disc.id);
+
+    this.selectedDisc = this.discs[this.disc.id]
+    
+    // Alter shared discs
+    this.discService.changeDisc(this.selectedDisc);
+    this.discService.changeDiscs(this.discs);
+
+    // Set local current discs
+    this.curDisc = this.selectedDisc;
+    this.curDiscs = this.discs;
+
+    this.router.navigateByUrl('/albumdetail/' + this.disc.id);
 
     this.feedbackFormDirective.resetForm();
     this.discsearchForm.reset({
       id: 0
     });
+
   }
 
 }
