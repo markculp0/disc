ebaf9e53d057caa73976ff6364cc9920b66c46d7
Mark Culp <markculp0@gmail.com>
2020-07-11 22:45:59 -0500

    user class

---
+++ src/app/shared/user.ts
@@ -0,0 +1,10 @@
+
+/**
+ * User class - Defines User type
+ */
+export class User {
+    /** username - User identity */
+    username: string;
+    /** password - Authentication mechanism */
+    password: string;
+}
---
+++ src/app/shared/users.ts
@@ -0,0 +1,18 @@
+
+import { User } from './user';
+
+export const USERS: User[] = 
+[
+    {
+        username: 'user1',
+        password: 'pass1'
+    },
+    {
+        username: 'user2',
+        password: 'pass2'
+    },
+    {
+        username: 'user3',
+        password: 'pass3'
+    }
+];
